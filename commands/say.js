0c57c8a1562cc50e381c56d313b7a58707b985eb
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-14 00:48:16 -0300

    Nova função
    Agora pode especificar em qual chat (usando o ID) deseja enviar a mensagem.

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,16 @@
 const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
-  let botmessage = args.join(" ");
+  
+  args = args.shift();
+  
+    let chat = client.channels.find("id", args[0]);
+    if(chat) {
+        chat.send(args.shift().join(" "));
+    } else {
+    let botmessage = args.join(" ");
   message.channel.send(botmessage);
 }
 
