da5de9ae4ce0a384bff00089a97ab4062bb90e2a
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-29 19:49:45 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,11 +3,15 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
+  let msgq = ("mensagens")
+  if(!args[0] || args[0 == "1"]) return
+  let msgq = ("mensagem")
+  
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "Gerenciar Mensagens");
   if(!args[0]) return errors.noMsgQuantity();
   message.delete();
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`${args[0]} mensagens deletadas.`).then(msg => msg.delete(5000));
+    message.channel.send(`${args[0]} ${msgq} deletadas.`).then(msg => msg.delete(5000));
   });
 }
 
