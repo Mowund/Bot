43d958071010b4478e4aaa4caef7d3bcf14196ec
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-29 20:35:34 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => {
 
   let msgq = ("mensagens");
-  if(!args[0] || args[0 == "1"]) return;
+  if(args[0 == "1"]) return;
   msgq = ("mensagem");
   
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "Gerenciar Mensagens");
