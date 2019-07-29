e3e4e955475e9df16495dde9117959bdee5aab43
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-29 11:02:39 -0300

    Delete ready.ts

--- listeners/client/ready.ts
+++
@@ -1,7 +0,0 @@
-import { CustomListener } from '../../classes/listener';
-
-export default class extends CustomListener {
-	public async exec(): Promise<void> {
-		console.log(this.client.user!.tag);
-	}
-}
