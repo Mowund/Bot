bc073e2a9cc0260caf612dc8abe0f8b5e27cfabe
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-30 20:28:35 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
   
   message.channel
     .bulkDelete(args[0])
-    .then(() => {message.channel.send(`${msgq}.`).then(msg => msg.delete(5000));
+    .then(() => {message.channel.send(`${msgq}.`).then(msg => msg.delete(5000))
     .catch(console.error);
   });
 }
