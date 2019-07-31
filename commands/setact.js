086cbdf2106667c880b176ed65b9261087670724
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-30 23:30:54 -0300

    Update setact.js

--- commands/setact.js
+++ commands/setact.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_WEBHOOKS")) return errors.noPerms(message, "Gerenciar Webhooks");
     
     var actype = args[0].toUpperCase();
-    var act = args.slice(1).join;
+    var act = args.slice(1).join(" ");
     
     bot.user.setActivity(act, {type: actype});
 
