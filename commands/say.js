488b7e81395eca918018d5632131c172802cbaa8
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-31 17:47:49 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "Gerenciar Mensagens");
   
   if(args[0] === "/tts") {
-  if(!message.member.hasPermission("SEND_TTS_MESSAGES")) { 
+  if(message.member.hasPermission("SEND_TTS_MESSAGES")) { 
   var tts = ("true");
   var bmsg = args.slice(1).join(" ");
   } else {
