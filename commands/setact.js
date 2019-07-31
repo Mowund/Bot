c7a03ea7e4c2b1a4f8d0c8a5394eda2a07a1b953
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-31 14:41:44 -0300

    Update setact.js

--- commands/setact.js
+++ commands/setact.js
@@ -3,11 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_WEBHOOKS")) return errors.noPerms(message, "Gerenciar Webhooks");
     
-    var actype = args[0].toUpperCase();
-    var act = args.slice(1).join(" ");
+        var actype = args[0].toUpperCase();
+        var act = args.slice(1).join(" ");
     
     bot.user.setActivity(act, {type: actype});
     message.channel.send("Feito")
+    
+    if(args[0] === "streaming")
+        var acturl = args[1]
+        act = args.slice(2).join(" ")
+    
+    client.user.setActivity(act, {type: actype, url: acturl});
 
 }
 
