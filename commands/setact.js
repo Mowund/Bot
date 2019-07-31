2cf34681f00851c703f6b41ca88afad3defb8a2c
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-30 23:22:33 -0300

    Update setact.js

--- commands/setact.js
+++ commands/setact.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "Administrador");
+    if(!message.member.hasPermission("MANAGE_WEBHOOKS")) return errors.noPerms(message, "Gerenciar Webhooks");
     
     var actype = args[0].toUpperCase();
     var act = args[1];
