2bc50e13f2e8bfd61c0e32cf86e9fd17adb3e05e
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-31 16:43:16 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
 let hEmbed = new Discord.RichEmbed()
     .setColor("#0000ff")
     .setTitle("**Comandos**")
-    .addField("Padrões", `${pr}`)
-    .addField("Staff", `${pr}`);
+    .addField("Padrões", `${pr}botinfo\n${pr}help\n${pr}ping\n${pr}report\n${pr}serverinfo\n${pr}uptime`)
+    .addField("Staff", `${pr}addrole\n${pr}ban\n${pr}clear\n${pr}kick\n${pr}prefix\n${pr}removerole\n${pr}say\n${pr}setact`);
 
     message.channel.send(hEmbed);
 
