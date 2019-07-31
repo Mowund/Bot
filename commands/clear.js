da3c1ecd441fa8115a6b0494c1d532f526c8ca9b
Smidul <45880040+Smidul@users.noreply.github.com>
2019-07-31 15:53:59 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,22 +2,25 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
+
+  var msgc = parseInt(args[0]);
+  var msgt = Math.floor(msgc + 1);
   
   if(args[0] === "0") {
     var msgq = ("Impossível deletar 0 mensagens");
   } else if (args[0] === "1") {
     msgq = (`1 mensagem deletada`);
   } else {
-    msgq = (`${args[0]} mensagens deletadas`);
+    msgq = (`${msgc} mensagens deletadas`);
   };
   
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "Gerenciar Mensagens");
   if(!args[0]) return message.delete().then(errors.noMsgQuantity(message.channel));
   
-  message.channel.bulkDelete(args[0]);
-  message.delete()
-  .then(() => {message.channel.send(`${msgq}.`).then(msg => msg.delete(5000))
-  .catch(console.error)
+  message.channel
+    .bulkDelete(msgt)
+    .then(() => {message.channel.send(`${msgq}.`).then(msg => msg.delete(5000))
+    .catch(console.error);
   });
 }
 
