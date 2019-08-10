36deb2fe5684ec0d9cf073a34fae6b50badba53e
Smidul <45880040+Smidul@users.noreply.github.com>
2019-08-10 13:23:50 -0300

    Primeira versão do comando }change
    O comando }change para mudar cor foi lançado, está e a primeira versão dele e mais coisas virão pela frente.
    Além disso, a versão de testes do bot roda no mesmo bot, porém com um prefixo diferente, que é o >

---
+++ commands/change.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  var roleC = args[0].replace(/#/g, '');
+  if(roleC === "000000") {
+    roleC = "000001"
+  }
+
+  let roleN = `USER-${message.author.id}`;
+  const role = message.guild.roles.find(x => x.name == roleN);
+  if(!role) {
+    if(!roleC) {
+      message.channel.send("Uso correto: `}change {cor em hex}`")
+    } else {
+      message.guild.createRole({
+        name: roleN,
+        color: `${roleC}`,
+        position: 93
+      });
+      message.member.addRole(role.id).catch(err => console.error(err));
+    }
+  } else {
+    let cEmb = new Discord.RichEmbed()
+    .setColor(parseInt(roleC, 16))
+		.setFooter('Cor alterada')
+    .setImage(`https://dummyimage.com/150x50/${roleC}/000000&text=${roleC}`);
+    
+    role.setColor(roleC);
+    role.setPosition(93)
+    message.channel.send(cEmb);
+    message.member.addRole(role.id).catch(err => console.error(err));
+  }
+}
+
+module.exports.help = {
+  name:"change"
+}
