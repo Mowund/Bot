559dec3b672af91ea1c22e7c8b2617451f50bac3
Smidul <45880040+Smidul@users.noreply.github.com>
2019-08-11 10:00:42 -0300

    Update change.js
    • Agora o bot pode escolher uma cor aleatória usando "}change random".
    • Agora quando uma cor é inválida, o bot responderá com "Inválido".

--- commands/change.js
+++ commands/change.js
@@ -1,9 +1,15 @@
+const botconfig = require("../botconfig.json");
 const Discord = require("discord.js");
 var tinycolor = require("tinycolor2");
+let pr = (botconfig.prefix);
 
 module.exports.run = async (bot, message, args) => {
 
-  var roleC = args[0].replace(/#/g, '');
+  var rolePC = tinycolor(args.join(" ")).toHexString();
+  if(args[0] === "random") {
+    rolePC = tinycolor.random().toHexString();
+  }
+  var roleC = rolePC.replace(/#/g, '');
   if(roleC === "000000") {
     roleC = "000001"
   }
@@ -15,29 +21,39 @@ module.exports.run = async (bot, message, args) => {
 
   let roleN = `USER-${message.author.id}`;
   const role = message.guild.roles.find(x => x.name == roleN);
-  if(!role) {
-    if(!roleC) {
-      message.channel.send("Uso correto: `}change {cor em hex}`")
-    } else {
-      message.guild.createRole({
-        name: roleN,
-        color: `${roleC}`,
-        position: 93
-      });
+
+if(!args[0]) {
+  message.channel.send(`Uso correto: \`${pr}change {cor}\``)
+} else {
+  if(tinycolor(args.join(" ")).isValid() || args[0] === "random") {
+    if(!role) {
+        message.guild.createRole({
+          name: roleN,
+          color: `${roleC}`,
+          position: 93
+        });
+        message.member.addRole(role.id).catch(err => console.error(err));
+      
+       } else {
+      let cEmb = new Discord.RichEmbed()
+      .setColor(parseInt(roleC, 16))
+	  	.setFooter('Cor alterada')
+      .setImage(`https://dummyimage.com/150x50/${roleC}/${roleL}&text=${roleC}`);
+      
+      setTimeout(function(){
+        role.setColor(roleC)
+      }, 2500);
+      message.channel.send(cEmb);
       message.member.addRole(role.id).catch(err => console.error(err));
     }
   } else {
-    let cEmb = new Discord.RichEmbed()
-    .setColor(parseInt(roleC, 16))
-		.setFooter('Cor alterada')
-    .setImage(`https://dummyimage.com/150x50/${roleC}/${roleL}&text=${roleC}`);
-    
-    setTimeout(function(){
-      role.setColor(roleC)
-    }, 2500);
-    message.channel.send(cEmb);
-    message.member.addRole(role.id).catch(err => console.error(err));
-  }
+    let iEmb = new Discord.RichEmbed()
+      .setColor(000000)
+	  	.setFooter('Cor inválida')
+      .setImage(`https://dummyimage.com/150x50/000000/ff0000&text=Inválido`);
+      message.channel.send(iEmb);
+}
+}
 }
 
 module.exports.help = {
