d3351307c323200e93009bfd40b2622fb218e634
Smidul <45880040+Smidul@users.noreply.github.com>
2019-08-12 10:49:59 -0300

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -7,12 +7,11 @@ const nick = gNick.replace(/[[AFK\] ]/g, '')
 
 if(gNick === nick) {
     message.member.setNickname(`[AFK] ${nick}`)
-
+    message.channel.send(`O seu nick foi setado para \`[AFK] ${nick}\`.`)
 } else {
     message.member.setNickname(nick)
-
+    message.channel.send(`O seu nick foi setado para \`${nick}\`.`)
 }
-message.channel.send(`O seu nick foi setado para \`${message.member.displayName}\`.`)
 }
 
 module.exports.help = {
