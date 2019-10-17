4fbf9cec0de1f968a4f2472cd6a6b37bc33ef05d
Smidul <45880040+Smidul@users.noreply.github.com>
2019-10-17 11:57:20 -0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 let pack = require("../package.json");
 
 module.exports.run = async (bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
+    
     let botembed = new Discord.RichEmbed()
     .setTitle("**Informações do Bot**")
     .setColor("#15f153")
-    .setThumbnail(bicon)
+    .setThumbnail(bot.user.displayAvatarURL)
     .addField("Nome do Bot", bot.user.username, true)
     .addField("Versão do Bot", pack.version, true)
 
