ab5a7314df143aab040e92bf1491011e03a09862
Smidul <45880040+Smidul@users.noreply.github.com>
2020-09-26 03:51:39 -0300

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -3,7 +3,7 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var tmsg = args.join(' ');
+    var tmsg = args.join(' ').toLowerCase;
     var tmsg = tmsg.replace(/[a@]/g, function($1) {return $1 === 'a' ? '@' : 'a'})                                  
                    .replace(/[b;]/g, function($1) {return $1 === 'b' ? ';' : 'b'})
                    .replace(/[c']/g, function($1) {return $1 === 'c' ? '\'' : 'c'})
