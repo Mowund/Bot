4fee3866c71bd0fc7436862738eb4a27f8e4000c
Smidul <45880040+Smidul@users.noreply.github.com>
2020-09-26 02:30:38 -0300

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -4,8 +4,9 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => {
 
     var tmsg = args.join(' ');
-    var tmsg = tmsg.replace('a','@').replace('@','a')
-                   .replace('b',';').replace(';','b');
+    var tmsg = tmsg.replaceEach(tmsg, new String[]{"a", "@},
+                                      new String[]{"@", "a"}
+               )
 
     message.channel.send(tmsg);
 
