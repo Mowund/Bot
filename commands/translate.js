8a12e8666fa66cea44aeb240341dfeb10425971b
Smidul <45880040+Smidul@users.noreply.github.com>
2020-09-26 03:02:49 -0300

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -6,7 +6,15 @@ module.exports.run = async (bot, message, args) => {
     var tmsg = args.join(' ');
     var tmsg = tmsg.replace(/[a@]/g, function($1) {return $1 === 'a' ? '@' : 'a'})                                  
                    .replace(/[b;]/g, function($1) {return $1 === 'b' ? ';' : 'b'})
-                   .replace(/[c:]/g, function($1) {return $1 === 'c' ? ':' : 'c'});
+                   .replace(/[c']/g, function($1) {return $1 === 'c' ? '\'' : 'c'})
+                   .replace(/[d\$]/g, function($1) {return $1 === 'd' ? '$' : 'd'})
+                   .replace(/[e3]/g, function($1) {return $1 === 'e' ? '3' : 'e'})
+                   .replace(/[f%]/g, function($1) {return $1 === 'f' ? '%' : 'f'}).replace('_','f')
+                   .replace(/[g&]/g, function($1) {return $1 === 'g' ? '&' : 'g'})
+                   .replace(/[h-]/g, function($1) {return $1 === 'h' ? '-' : 'h'})
+                   .replace(/[i8]/g, function($1) {return $1 === 'i' ? '8' : 'i'})
+                   .replace(/[j\+]/g, function($1) {return $1 === 'j' ? '+' : 'j'})
+                   .replace(/[k\(]/g, function($1) {return $1 === 'k' ? '(' : 'k'});
                    
 
     message.channel.send(tmsg);
