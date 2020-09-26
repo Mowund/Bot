b8e623b72149a6b179c049c1e26315918d254390
Smidul <45880040+Smidul@users.noreply.github.com>
2020-09-26 03:11:31 -0300

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -14,9 +14,24 @@ module.exports.run = async (bot, message, args) => {
                    .replace(/[h-]/g, function($1) {return $1 === 'h' ? '-' : 'h'})
                    .replace(/[i8]/g, function($1) {return $1 === 'i' ? '8' : 'i'})
                    .replace(/[j\+]/g, function($1) {return $1 === 'j' ? '+' : 'j'})
-                   .replace(/[k\(]/g, function($1) {return $1 === 'k' ? '(' : 'k'});
+                   .replace(/[k\(]/g, function($1) {return $1 === 'k' ? '(' : 'k'})
+                   .replace(/[l\)]/g, function($1) {return $1 === 'l' ? ')' : 'l'})
+                   .replace(/[m\?]/g, function($1) {return $1 === 'm' ? '?' : 'm'})
+                   .replace(/[n\!]/g, function($1) {return $1 === 'n' ? '!' : 'n'})
+                   .replace(/[o9]/g, function($1) {return $1 === 'o' ? '9' : 'o'})
+                   .replace(/[p0]/g, function($1) {return $1 === 'p' ? '0' : 'p'})
+                   .replace(/[q1]/g, function($1) {return $1 === 'q' ? '1' : 'q'})
+                   .replace(/[r4]/g, function($1) {return $1 === 'r' ? '4' : 'r'})
+                   .replace(/[s#]/g, function($1) {return $1 === 's' ? '#' : 's'})
+                   .replace(/[t5]/g, function($1) {return $1 === 't' ? '5' : 't'})
+                   .replace(/[u7]/g, function($1) {return $1 === 'u' ? '7' : 'u'})
+                   .replace(/[v:]/g, function($1) {return $1 === 'v' ? ':' : 'v'})
+                   .replace(/[w2]/g, function($1) {return $1 === 'w' ? '2' : 'w'})
+                   .replace(/[x"]/g, function($1) {return $1 === 'x' ? '\"' : 'x'})
+                   .replace(/[y6]/g, function($1) {return $1 === 'y' ? '6' : 'y'})
+                   .replace(/[z\*]/g, function($1) {return $1 === 'z' ? '*' : 'z'});
                    
-
+                  
     message.channel.send(tmsg);
 
 }
