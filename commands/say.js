d0bc7ab602e64bb5f88f6813a3c850f15d020e35
Smidul <45880040+Smidul@users.noreply.github.com>
2020-12-03 19:11:17 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -8,26 +8,26 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "Gerenciar Mensagens");
 
   let guild = bot.guilds.get(args[0])
+  var bmsg = args.join(' ');
 
   if (guild) {
   channel = guild.channels.get(args[1]);
-  var bmsg = args.slice(2).join(" ");
+  bmsg = args.slice(2).join(" ");
   } 
   else if (message.guild.channels.get(args[0])) {
     var channel = message.guild.channels.find(c => c.id === args[0])
-    var bmsg = args.slice(1).join(" ");
+    bmsg = args.slice(1).join(" ");
   }
 
   if(args[0] === "/tts") {
     if(message.member.hasPermission("SEND_TTS_MESSAGES")) { 
       var tts = ("true");
-      var bmsg = args.slice(1).join(" ");
+      bmsg = args.slice(1).join(" ");
     } else {
       errors.noPerms(message, "Enviar Mensagens em TTS")
     };
   } else {
     tts = ("false");
-    bmsg = args.join(' ');
   };
   
   if (channel) {
