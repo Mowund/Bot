f7744880a827be9640f7cfc076d0aafcde10aafc
Smidul <45880040+Smidul@users.noreply.github.com>
2020-12-03 10:13:35 -0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-let pack = require("../package.json");
+let pack = require('../package.json');
 
 module.exports.run = async (bot, message, args) => {
     
@@ -7,8 +7,9 @@ module.exports.run = async (bot, message, args) => {
     .setTitle('**Informações do Bot**')
     .setColor('#00ff55')
     .setThumbnail(bot.user.displayAvatarURL)
-    .addField('Nome do Bot', bot.user.username, true)
+    .addField('Nome do Bot', bot.user.username)
     .addField('Versão do Bot', pack.version, true)
+    .addField('Entrou em', bot.user.joinedAt)
     .addField('Convites', '[Servidor de Comunidade](https://discord.gg/f85rEGJ)\nBot privado, não é possível convidá-lo.');
   
     message.channel.send(botembed);
