9aa3de96fc7759c8afe4d7353d6fd8eed5514333
Smidul <redstonefernandes2015@gmail.com>
2021-01-02 07:02:19 -0300

    Simplificação

--- utils/utils.js
+++ utils/utils.js
@@ -16,7 +16,7 @@ module.exports.msgEdit = async (chan, id, medit) => {
 
 module.exports.diEmb = (eMsg, msg, color, title, diB, diL, diT, desc) => {
 
-    var pEmb = new Discord.MessageEmbed()
+    var emb = new Discord.MessageEmbed()
     .setColor(parseInt(color, 16))
     .setTitle(title)
     .setImage(`https://dummyimage.com/300x100/${diB}/${diL}&text=+${diT}`)
@@ -24,20 +24,14 @@ module.exports.diEmb = (eMsg, msg, color, title, diB, diL, diT, desc) => {
     .setTimestamp(Date.now());
 
     if(desc) {
-        pEmb = new Discord.MessageEmbed()
-        .setColor(parseInt(color, 16))
-        .setTitle(title)
-        .setDescription(desc)
-        .setImage(`https://dummyimage.com/300x100/${diB}/${diL}&text=+${diT}`)
-        .setFooter(`Solicitado por ${msg.author.username}`, msg.author.avatarURL())
-        .setTimestamp(Date.now());
+        emb = emb.setDescription(desc)
     };
 
     if(eMsg === 0) {
-        return pEmb
+        return emb
     } else if(eMsg === 1) {
-        msg.channel.send(pEmb)
+        msg.channel.send(emb)
     } else {
-        eMsg.edit(pEmb)
+        eMsg.edit(emb)
     };
 }
