019417ec304ab6f5889829b0bf51cda4507b5ebb
Bryan Fernandes <45880040+Smidul@users.noreply.github.com>
2021-08-13 23:22:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const Discord = require('discord.js');
 const fs = require('fs');
 let coins = require('./coins.json');
 let xp = require('./xp.json');
-let env = require('./env.json') ?? process.env;
 require('colors');
 require('log-timestamp');
 const WOKCommands = require('wokcommands');
 require('dotenv').config();
+
+var env;
+try {
+  env = require('./env.json');
+} catch {
+  env = process.env;
+}
 
 const client = new Discord.Client({
   intents: ['GUILDS', 'GUILD_MESSAGES'],
