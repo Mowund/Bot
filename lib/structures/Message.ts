8237307622e5b2c0453edc279f35e11510fef72b
Bryan Fernandes <45880040+Smidul@users.noreply.github.com>
2022-11-08 22:33:42 -0300

    Remove unused structures

--- lib/structures/Message.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars */
-
-import { Message } from 'discord.js';
-import { AppUser } from './User';
-
-export interface AppMessage extends Message {
-  author: AppUser;
-}
--- lib/structures/User.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars */
-
-import { User } from 'discord.js';
-
-export interface AppUser extends User {
-  lastScamTimestamp?: number;
-}
